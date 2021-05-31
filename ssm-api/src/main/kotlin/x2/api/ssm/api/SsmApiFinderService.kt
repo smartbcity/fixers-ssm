d4dc7b3c1409131dbac476f4ee0bff99434800d6
Luc Heydel <luc@smartb.city>
2021-05-31 16:54:21 +0200

    first endpoints

--- ssm-api/src/main/kotlin/x2/api/ssm/api/SsmApiFinderService.kt
+++ ssm-api/src/main/kotlin/x2/api/ssm/api/SsmApiFinderService.kt
@@ -1,17 +1,33 @@
 package x2.api.ssm.api
 
-import f2.dsl.function.F2Function
-import f2.function.spring.adapter.f2Function
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Service
+import ssm.coucbdb.dsl.query.CdbGetSsmListQueryFunction
+import ssm.dsl.query.SsmGetQueryFunction
+import ssm.dsl.query.SsmGetSessionQueryFunction
+import ssm.dsl.query.SsmListSessionQueryFunction
+import ssm.dsl.query.SsmListSsmQueryFunction
 
 @Service
 class SsmApiFinderService(
+	private val cdbGetSsmListQueryFunction: CdbGetSsmListQueryFunction,
+	private val ssmGetQueryFunction: SsmGetQueryFunction,
+	private val ssmGetSessionQueryFunction: SsmGetSessionQueryFunction,
+	private val ssmListSsmQueryFunction: SsmListSsmQueryFunction,
+	private val ssmListSessionQueryFunction: SsmListSessionQueryFunction
 ) {
+	@Bean
+	fun getAllSsm() = cdbGetSsmListQueryFunction
 
 	@Bean
-	fun test(): F2Function<Unit, String> = f2Function {
-		println("test")
-		"test"
-	}
+	fun getAllSsmIds() = ssmListSsmQueryFunction
+
+	@Bean
+	fun getSsm() = ssmGetQueryFunction
+
+	@Bean
+	fun getAllSessionIds() = ssmListSessionQueryFunction
+
+	@Bean
+	fun getSession() = ssmGetSessionQueryFunction
 }
