eced569741775cf3f05b62b748a24c071c68f2e7
Luc Heydel <luc@smartb.city>
2021-06-10 12:50:05 +0200

    fix getSessionLogs

--- ssm-f2/ssm-f2-query/src/main/kotlin/ssm/f2/query/SsmGetSessionLogsQueryFunctionImpl.kt
+++ ssm-f2/ssm-f2-query/src/main/kotlin/ssm/f2/query/SsmGetSessionLogsQueryFunctionImpl.kt
@@ -1,10 +1,10 @@
 package ssm.f2.query
 
-import kotlinx.coroutines.future.await
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import ssm.dsl.query.SsmGetSessionLogsQueryFunction
 import ssm.dsl.query.SsmGetSessionLogsQueryResult
+import ssm.f2.commons.awaitInstances
 import ssm.f2.commons.ssmF2Function
 
 @Configuration
@@ -13,7 +13,7 @@ class SsmGetSessionLogsQueryFunctionImpl {
     @Bean
     fun ssmGetSessionLogsQueryFunction(): SsmGetSessionLogsQueryFunction = ssmF2Function { cmd, ssmClient ->
         ssmClient.log(cmd.session)
-            .await()
+            .awaitInstances()
             .let(::SsmGetSessionLogsQueryResult)
     }
 }
