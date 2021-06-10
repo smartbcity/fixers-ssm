6b3963651df2fdac5db35c7a4dc8835549c86945
Luc Heydel <luc@smartb.city>
2021-06-10 12:53:10 +0200

    fix typing

--- ssm-f2/ssm-f2-session-perform-action/src/main/kotlin/ssm/f2/SsmSessionPerformActionCommand.kt
+++ ssm-f2/ssm-f2-session-perform-action/src/main/kotlin/ssm/f2/SsmSessionPerformActionCommand.kt
@@ -6,7 +6,7 @@ import f2.dsl.function.F2FunctionRemote
 import ssm.client.sign.model.Signer
 import ssm.dsl.InvokeReturn
 import ssm.dsl.SsmCommand
-import ssm.dsl.SsmContext
+import ssm.dsl.SsmContextBase
 
 typealias SsmSessionPerformActionFunction = F2Function<SsmSessionPerformActionCommand, SsmSessionPerformActionResult>
 typealias SsmSessionPerformActionRemoteFunction = F2FunctionRemote<SsmSessionPerformActionCommand, SsmSessionPerformActionResult>
@@ -18,7 +18,7 @@ class SsmSessionPerformActionCommand(
 		override val bearerToken: String?,
 		val signer: Signer,
 		val action: String,
-		val context: SsmContext
+		val context: SsmContextBase
 ): SsmCommand
 
 
