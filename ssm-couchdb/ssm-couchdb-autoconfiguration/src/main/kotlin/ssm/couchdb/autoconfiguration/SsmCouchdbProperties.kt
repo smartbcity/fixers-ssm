337cb918e0f31c553938d803e5513c88861ff71d
Luc Heydel <luc@smartb.city>
2021-06-14 14:07:51 +0200

    multiple cdb config

--- ssm-couchdb/ssm-couchdb-autoconfiguration/src/main/kotlin/ssm/couchdb/autoconfiguration/SsmCouchdbProperties.kt
+++ ssm-couchdb/ssm-couchdb-autoconfiguration/src/main/kotlin/ssm/couchdb/autoconfiguration/SsmCouchdbProperties.kt
@@ -1,14 +1,11 @@
 package ssm.couchdb.autoconfiguration
 
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
-
 @ConstructorBinding
-@ConfigurationProperties(prefix = "ssm.couchdb")
 data class SsmCouchdbProperties(
-	val url: String,
-	val username: String,
-	val password: String,
-	val serviceName: String,
+    val url: String,
+    val username: String,
+    val password: String,
+    val serviceName: String,
 )
