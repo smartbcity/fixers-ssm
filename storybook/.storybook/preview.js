0c6907f0ad0b15ccb2f59b14430671540c3c8c01
Adrien Poupard <adrien@smartb.city>
2021-08-13 15:56:46 +0200

    Use archetype storybook canvas

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -1,8 +1,19 @@
-import {Canvas} from "./Canvas"
+import { StorybookCanvas } from "@smartb/archetypes-ui-documentation";
 
 export const parameters = {
+  // options: {
+  //   storySort: {
+  //     order: [
+  //       "Overview",
+  //       ["Getting started", "Domain"],
+  //       "Swagger",
+  //       "View",
+  //       "S2"
+  //     ]
+  //   }
+  // },
   docs: {
-    container: Canvas
+    container: StorybookCanvas
   },
 }
 
--- storybook/.storybook/webpack.config.js
+++ storybook/.storybook/webpack.config.js
@@ -4,6 +4,9 @@ import {mdx} from '/node_modules/@mdx-js/react'
 `
 
 module.exports = ({config}) => {
-    config.module.rules[2].use[1].options = {...config.module.rules[2].use[1].options, renderer}
+    config.module.rules[2].use[1].options = {
+        ...config.module.rules[2].use[1].options,
+        renderer
+    }
     return config;
 }
