c11f1a083aa9982c0fb36ecb5be1219d32b75cba
Adrien Poupard <adrien@smartb.city>
2022-01-07 23:35:09 +0100

    Sample use stable version

--- sample/ssm-full-ext/build.gradle.kts
+++ sample/ssm-full-ext/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 dependencies {
 	api("org.springframework.boot:spring-boot-starter-webflux:${city.smartb.gradle.dependencies.FixersVersions.Spring.boot}")
-	implementation("city.smartb.ssm:ssm-data-spring-boot-starter:experimental-SNAPSHOT")
-	implementation("city.smartb.ssm:ssm-tx-spring-boot-starter:experimental-SNAPSHOT")
+	implementation("city.smartb.ssm:ssm-data-spring-boot-starter:0.2.1")
+	implementation("city.smartb.ssm:ssm-tx-spring-boot-starter:0.2.1")
 
 }
